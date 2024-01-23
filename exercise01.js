f42fae3ef0cc16e0c04856e7e7b686ec7711f9a9
asGabriel <andrade.gabriel2295@gmail.com>
2024-01-22 22:51:22 -0300

    feat: solve exercise01

---
+++ exercise01.js
@@ -0,0 +1,9 @@
+const length = 10;
+const width = 5;
+
+const perimeter = (length, width) => {
+    const calculatedPerimeter = (length * width);
+    return calculatedPerimeter;
+}
+
+console.log(perimeter(length, width));
