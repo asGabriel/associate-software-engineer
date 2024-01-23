8259938c4676a4068868cd17b5bd49250ec41ecd
asGabriel <andrade.gabriel2295@gmail.com>
2024-01-23 00:09:38 -0300

    feat: solve exercise06.js

---
+++ exercise06.js
@@ -0,0 +1,15 @@
+const color = "black";
+
+switch (color) {
+    case "red":
+        console.log("drupas");
+        break;
+
+    case "green":
+        console.log("cítrica");
+        break;
+        
+    default:
+        console.log("Color not found")
+        break;
+}
