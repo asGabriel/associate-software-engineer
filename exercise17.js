99eeaa9897cab782d6660535d7b0b90d0c83a9d7
asGabriel <andrade.gabriel2295@gmail.com>
2024-01-28 10:39:17 -0300

    feat: done exercise17

---
+++ exercise17.js
@@ -0,0 +1,12 @@
+function getSum(value1, value2) {
+    return value1 + value2;
+};
+
+function printResult() {
+    const value1 = 10;
+    const value2 = 20;
+
+    console.log(getSum(value1, value2));
+};
+
+printResult();
