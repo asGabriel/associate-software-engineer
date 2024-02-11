e2913b7204224f33cfce9adc45f9b81f5071f70c
asGabriel <andrade.gabriel2295@gmail.com>
2024-02-11 11:18:12 -0300

    fix: remove console.log

--- software-engineer-I/fc-1/javascript/modulo-7/topico-1/exercise07.js
+++ software-engineer-I/fc-1/javascript/modulo-7/topico-1/exercise07.js
@@ -1,6 +1,5 @@
 const itensHTML = document.querySelectorAll(".item");
 
-console.log(itensHTML);
 for (let index = 0; index < itensHTML.length; index++) {
     const item = itensHTML[index];
     item.style.backgroundColor = "blue"
