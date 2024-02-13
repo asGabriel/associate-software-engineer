94b57174a255e3a9cad98340a8fc307cbf39a04c
asGabriel <andrade.gabriel2295@gmail.com>
2024-02-13 15:31:55 -0300

    fix: variable name

--- software-engineer-I/fc-1/javascript/modulo-7/topico-2/exercise03.js
+++ software-engineer-I/fc-1/javascript/modulo-7/topico-2/exercise03.js
@@ -1,6 +1,6 @@
-const button = document.getElementById("buttonToChangeBackgroundColor");
+const buttonChangeColorHTML = document.getElementById("buttonToChangeBackgroundColor");
 
-button.addEventListener("click", () => {
+buttonChangeColorHTML.addEventListener("click", () => {
   const randomColor = "#" + (Math.floor(Math.random() * 900000) + 100000);
   document.body.style.backgroundColor = randomColor;
 });
