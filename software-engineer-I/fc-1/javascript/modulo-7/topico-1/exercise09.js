d7bd47f36adf854137c34534f97a65c9ac114629
asGabriel <andrade.gabriel2295@gmail.com>
2024-02-13 08:13:45 -0300

    feat: done exercise09

---
+++ software-engineer-I/fc-1/javascript/modulo-7/topico-1/exercise09.js
@@ -0,0 +1,6 @@
+const spanHTML = document.getElementsByTagName("span");
+
+for (let index = 0; index < spanHTML.length; index++) {
+    const span = spanHTML[index];
+    span.style.fontStyle = "italic"
+}
