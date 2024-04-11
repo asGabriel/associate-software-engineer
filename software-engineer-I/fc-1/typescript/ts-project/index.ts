b576147ecc43d865327d2a00072b26694a950448
asGabriel <andrade.gabriel2295@gmail.com>
2024-04-11 16:47:32 -0300

    fix: error on hello function

--- software-engineer-I/fc-1/typescript/ts-project/index.ts
+++ software-engineer-I/fc-1/typescript/ts-project/index.ts
@@ -2,4 +2,4 @@ export const hello = (who: string) => console.log(`Hello ${who}!`);
 
 
 hello("olaaa 123") // vai dar bom
-hello(123) // vai dar erro
+hello((123 as unknown) as string) // vai dar erro
